bc848830b3db1ee937d640ba42524e79d41b3a88
rajshekharpro <raj@shekharpro.com>
2024-08-07 14:09:03 +0530

    logging

---
+++ src/logger.py
@@ -0,0 +1,26 @@
+import logging
+import sys
+import os
+from datetime import datetime
+import exception
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+
+)
+
+# if __name__ == "__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info("Divide by zero")
+#         raise exception.CustomException(e, sys)
